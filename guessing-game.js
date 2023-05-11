61d2576600c9b3638f0bac76eb54d59824a108be
Gavin Musgrove <gavin.musgrove87@gmail.com>
2023-05-11 17:51:40 -0500

    mostly done

--- guessing-game.js
+++ guessing-game.js
@@ -9,11 +9,34 @@ const r1 = readline.createInterface({
 });
 let min = 0;
 let max = 0;
+let secretNumer = 0;
+let numAttempts = 5;
 
 const randomInRange = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+const askRepeat = () => {
+    const repeatYesOrNo = (answer) => {
+        if (!answer.toLowerCase() === 'yes'){
+            console.log('Ok! Bai!');
+            r1.close();
+            return false;
+          }
+        if (answer.toLowerCase() === 'yes') {
+          askLimit();
+        }
+      };
+    r1.question("Would you like to play again? ",repeatYesOrNo)
+}
+const askLimit = () => {
+   const limitAsk = (limit) => {
+    numAttempts = limit;
+    askRange()
+   }
+   r1.question("How attempts would you like? ",limitAsk)
 }
 
 const askRange = () => {
@@ -25,41 +48,51 @@ const askRange = () => {
     }
     const secondAnswer = (num) => {
         min = Number(num);
+        secretNumer = randomInRange(min, max);
         askGuess();
     }
     r1.question("Enter a max number: ", firstAnswer)
 
 }
 
-let secretNumer = randomInRange(min, max);
 
 
 const checkGuess = (num) => {
 
     if (Number(num) > secretNumer){
         console.log("Too high.")
+        numAttempts--;
         return false;
 
     } else if (Number(num) < secretNumer) {
         console.log("Too low.")
+        numAttempts--;
         return false;
     } else if (Number(num) === secretNumer) {
         console.log("Correct!!")
-        r1.close();
-        return true;
+        //r1.close();
+       // return true;
+       askRepeat();
 
     }
 
 };
 
 const askGuess = (guess) => {
- r1.question("Make your guess! ", (guess) =>{
-    if (!checkGuess(guess)) {
-        askGuess();
+
+    if (numAttempts === 0){
+        r1.close()
+        console.log('You lose!');
+        askRepeat();
     }
+    r1.question("Make your guess! ", (guess) =>{
+        if (!checkGuess(guess)) {
+            askGuess();
+    }
+
  })
 };
 
 
 
-askRange()
+askLimit()
